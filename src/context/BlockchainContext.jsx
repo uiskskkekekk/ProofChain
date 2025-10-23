1eb5a7ebd3e10008187a2e8ca587344d454fcbec
uiskskkekekk <angus.lu0611@gmail.com>
2025-10-23 19:59:41 +0800

    add blockchain context

---
+++ src/context/BlockchainContext.jsx
@@ -0,0 +1,49 @@
+import { ethers } from 'ethers';
+import { createContext, useContext, useState } from 'react';
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from '../utils/constants';
+
+const BlockchainContext = createContext();
+
+export const BlockchainProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [proofChainContract, setProofChainContract] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const connectWallet = async () => {
+    try {
+      if (!window.ethereum) return alert("Please install MetaMask");
+
+      setLoading(true);
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setCurrentAccount(accounts[0]);
+      
+      // After connecting wallet, initialize Ethers and contract
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+      
+      setProofChainContract(contract);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      throw new Error("Failed to connect wallet");
+    }
+  };
+
+  return (
+    <BlockchainContext.Provider 
+      value={{ 
+        loading,
+        currentAccount, 
+        connectWallet,
+        proofChainContract // Needed by notarization and verification components
+      }}
+    >
+      {children}
+    </BlockchainContext.Provider>
+  );
+};
+
+// Hook to access
+export const useBlockchain = () => useContext(BlockchainContext);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,41 @@
-// 這裡放地址跟ABI 我弄就好
+export const CONTRACT_ADDRESS = "0xC784f504Db552259078220d89F09bE7776c96c67";
+
+export const CONTRACT_ABI = [
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    name: "records",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_fileHash",
+        type: "bytes32",
+      },
+    ],
+    name: "storeProof",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
